2fcaf502ff656a4b40f1256aa209304f80dd6171
Adam Obeng <github@binaryeagle.com>
2016-09-15 16:54:14 +0100

    Add library fiiles for pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+import 🐍
+
+setuptools.setup(name='🐍',
+                 version=🐍 .__version__,
+                 description='🐍',
+                 url='https://github.com/adamobeng/snake',
+                 author='😎',
+                 author_email='snake@binaryeagle.com',
+                 packages=['🐍'],
+                 zip_safe=False,
+                 platforms=['any'],
+                 )
