2e47cbfae277cb99a0813e904c13fb435e66058b
Ducksual <ducksual@gmail.com>
2016-09-16 06:26:34 +0100

    Added optional download of HTML (it asks before downloading) bugfixes for windows unicode (UTF8 is not default in windows so it fails if you don't specify the encoding - I think this might be changing in python 3.6?)

--- get_emoji_table.py
+++ get_emoji_table.py
@@ -1,12 +1,24 @@
+import os
 import bs4
 import re
+import requests
+
 
 outpath = 'full-emoji-list.tsv'
+emojihtml = 'full-emoji-list.html'
+emojiurl = 'http://unicode.org/emoji/charts/full-emoji-list.html'
 
-soup = bs4.BeautifulSoup(open('snake/full-emoji-list.html'))
+if not os.path.isfile(emojihtml):
+    do_download = input("Emoji list not found, download from unicode.org (~26mb)? (y/N)")
+    if do_download.lower() == 'y':
+        emojireq = requests.get(emojiurl)
+        with open(emojihtml, 'wb') as op:
+            op.write(emojireq.content)
+
+soup = bs4.BeautifulSoup(open(emojihtml, encoding='utf8'), 'html.parser')
 table = soup.find_all('table')[0]
 
-out = open(outpath, 'w')
+out = open(outpath, 'w', encoding='utf8')
 for row in table.find_all('tr'):
     out.write(
             '\t'.join([c.text for c in row.find_all(re.compile('th|td'))]) + '\n'
