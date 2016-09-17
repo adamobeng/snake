c6c82231a24dd9c1b3d7f54060e90f35a5147143
Ducksual <ducksual@gmail.com>
2016-09-17 07:39:05 +0100

    convert_file code moved to python_to_emoji, removed convert_file.py

--- python_to_emoji.py
+++ python_to_emoji.py
@@ -1,9 +1,12 @@
 from reversible_emojidict import EmojiMapper
 import sys
+import os
 import replace_emoji
+from tempfile import mkstemp
 import tokenize
 
 REPLACEMENTS = EmojiMapper()
+
 
 def replace_keywords(token_list):
 
@@ -11,9 +14,9 @@ def replace_keywords(token_list):
     for t in token_list:
         if one:
             one = False
-            next
+            continue
 
-        if (t.string) in REPLACEMENTS:
+        if t.string in REPLACEMENTS:
             yield tokenize.TokenInfo(
                 type=t.type,
                 string=REPLACEMENTS[t.string],
@@ -25,9 +28,10 @@ def replace_keywords(token_list):
         else:
             yield t
 
+
 def replace_text(token_list):
-    #Replaces python keywords in tokenized python
-    #returns tokens with emojis placed
+    # Replaces python keywords in tokenized python
+    # returns tokens with emojis placed
     token_list = replace_keywords(token_list)
     token_list = list(token_list)
 
@@ -36,3 +40,27 @@ def replace_text(token_list):
     return token_list
 
 
+def python_to_emoji(inpath, outpath, logfile=None):
+
+    with open(inpath, 'rb') as infile, \
+            open(outpath, 'w', encoding='utf8') as outfile:
+        tokens = tokenize.tokenize(infile.readline)
+        emojified = replace_text(tokens)
+
+        # Write the emoji data to a log file if required
+        if logfile:
+            with open(logfile, 'w', encoding='utf8') as lf:
+                lf.write(str(emojified))
+
+        emojicode = tokenize.untokenize(emojified)
+
+        if logfile:
+            print(emojicode)
+
+        outfile.write(emojicode)
+
+if __name__ == '__main__':
+    if 3 <= len(sys.argv) <= 4:
+        python_to_emoji(*sys.argv[1:])
+    else:
+        sys.exit('USAGE: python_to_emoji inputfilepath outputfilepath (logfile)')
