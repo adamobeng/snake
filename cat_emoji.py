01643cb41edec677be9d6f875c79969685c771c3
Ducksual <ducksual@gmail.com>
2016-09-17 07:53:30 +0100

    Cat Function - Emoji Edition

---
+++ cat_emoji.py
@@ -0,0 +1,21 @@
+"""Basic cat in python - with extra emojis"""
+
+
+def 🐈(⭐args, encoding◀🚫):
+    🍀 fn 👇 args:
+
+        meow ◀ b''
+        f ◀ 📖(fn, 'rb')
+        🙏:
+            meow += f.read()
+        🤘:
+            f.📕()
+
+        ❔ encoding:
+            meow.decode(encoding)
+
+        return meow
+
+
+def onelinecat(⭐args):
+    return b''.join(📖(fn, 'rb').read() 🍀 fn 👇 args)
